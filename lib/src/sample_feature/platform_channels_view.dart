63be1bf89ea96cf66103929d4979a19cb36aa09f
Mark <Mark@123.c>
2023-05-06 14:27:46 +0800

    add win32 channel

--- lib/src/sample_feature/platform_channels_view.dart
+++ lib/src/sample_feature/platform_channels_view.dart
@@ -1,12 +1,94 @@
+import 'dart:async';
+import 'dart:ffi';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 // 参考：https://flutter.cn/docs/development/platform-integration/platform-channels
 
 /// Displays detailed information about a SampleItem.
-class PlatformChannelsView extends StatelessWidget {
+class PlatformChannelsView extends StatefulWidget {
   const PlatformChannelsView({super.key});
+  @override
+  State<PlatformChannelsView> createState() => _PlatformChannelsViewState();
 
   static const routeName = '/platform_channels_view';
+}
+
+class _PlatformChannelsViewState extends State<PlatformChannelsView> {
+  // [1] domain前缀 + channel名称
+  static const testChannel = MethodChannel('test');
+  static const batteryChannel = MethodChannel('samples.flutter.dev/battery');
+
+  String _testChannel = '';
+  String _batteryLevel = '';
+
+  // [2] channel方法
+  double ms12Avg = 0, ms34Avg = 0;
+  int msCnt = 0;
+  double ms12Max = 0, ms34Max = 0;
+  Future<void> _getChannelPerf() async {
+    final Future f1 = testChannel.invokeMethod('getSystemClock');
+    final Future f2 = testChannel.invokeMethod('getSystemClock');
+    final List<int> t1 = await f1;
+    final List<int> t2 = await f2;
+    final double ms12 = (t2[0] - t1[0]) * 1000.0 + (t2[1] - t1[1]) / 1000000.0;
+
+    final List<int> t3 = await testChannel.invokeMethod('getSystemClock');
+    final List<int> t4 = await testChannel.invokeMethod('getSystemClock');
+    final double ms34 = (t4[0] - t3[0]) * 1000.0 + (t4[1] - t3[1]) / 1000000.0;
+
+    ms12Avg = ms12Avg / (msCnt + 1) * msCnt + ms12 / (msCnt + 1);
+    ms34Avg = ms34Avg / (msCnt + 1) * msCnt + ms34 / (msCnt + 1);
+    ++msCnt;
+    ms12Max = max(ms12Max, ms12);
+    ms34Max = max(ms34Max, ms34);
+
+    setState(() {
+      _testChannel = "count: $msCnt\n"
+          "async: ${ms12.toStringAsFixed(3)}  avg: ${ms12Avg.toStringAsFixed(3)}  max: ${ms12Max.toStringAsFixed(3)} (ms)\n"
+          "await: ${ms34.toStringAsFixed(3)}  avg: ${ms34Avg.toStringAsFixed(3)}  max: ${ms34Max.toStringAsFixed(3)} (ms)";
+    });
+  }
+
+  Future<void> _getBatteryLevel() async {
+    String batteryLevel;
+    try {
+      final int result = await batteryChannel.invokeMethod('getBatteryLevel');
+      batteryLevel = 'Battery level at $result % .';
+    } on PlatformException catch (e) {
+      batteryLevel = "Failed to get battery level: '${e.message}'.";
+    }
+    setState(() {
+      _batteryLevel = batteryLevel;
+    });
+  }
+
+  Timer? _testTimer;
+
+  void startTimerPerf() {
+    _testTimer = Timer.periodic(const Duration(milliseconds: 10), (timer) {
+      _getChannelPerf();
+    });
+  }
+
+  void stopTimerPerf() {
+    _testTimer?.cancel();
+  }
+
+  @override
+  void initState() {
+    startTimerPerf();
+    _getBatteryLevel();
+    super.initState();
+  }
+
+  @override
+  dispose() {
+    stopTimerPerf();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,8 +96,31 @@ class PlatformChannelsView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('PlatformChannelsView'),
       ),
-      body: const Center(
-        child: Text('platform_channels_view'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                if (_testTimer != null && _testTimer!.isActive) {
+                  stopTimerPerf();
+                } else {
+                  startTimerPerf();
+                }
+                setState(() {}); // update Text
+              },
+              child: Text(_testTimer != null && _testTimer!.isActive
+                  ? 'Stop Channel Perf'
+                  : 'Start Channel Perf'),
+            ),
+            Text(_testChannel),
+            ElevatedButton(
+              onPressed: _getBatteryLevel,
+              child: const Text('Get Battery Level'),
+            ),
+            Text(_batteryLevel),
+          ],
+        ),
       ),
     );
   }
--- windows/runner/flutter_window.cpp
+++ windows/runner/flutter_window.cpp
@@ -4,63 +4,121 @@
 
 #include "flutter/generated_plugin_registrant.h"
 
+#include <flutter/event_channel.h>
+#include <flutter/event_sink.h>
+#include <flutter/event_stream_handler_functions.h>
+#include <flutter/method_channel.h>
+#include <flutter/standard_method_codec.h>
+#include <windows.h>
+#include <memory>
+
+static int GetBatteryLevel()
+{
+    SYSTEM_POWER_STATUS status;
+    if (GetSystemPowerStatus(&status) == 0 || status.BatteryLifePercent == 255)
+    {
+        return -1;
+    }
+    return status.BatteryLifePercent;
+}
+
 FlutterWindow::FlutterWindow(const flutter::DartProject& project)
     : project_(project) {}
 
 FlutterWindow::~FlutterWindow() {}
 
 bool FlutterWindow::OnCreate() {
-  if (!Win32Window::OnCreate()) {
-    return false;
-  }
+    if (!Win32Window::OnCreate()) {
+        return false;
+    }
 
-  RECT frame = GetClientArea();
+    RECT frame = GetClientArea();
 
-  // The size here must match the window dimensions to avoid unnecessary surface
-  // creation / destruction in the startup path.
-  flutter_controller_ = std::make_unique<flutter::FlutterViewController>(
-      frame.right - frame.left, frame.bottom - frame.top, project_);
-  // Ensure that basic setup of the controller was successful.
-  if (!flutter_controller_->engine() || !flutter_controller_->view()) {
-    return false;
-  }
-  RegisterPlugins(flutter_controller_->engine());
-  SetChildContent(flutter_controller_->view()->GetNativeWindow());
+    // The size here must match the window dimensions to avoid unnecessary surface
+    // creation / destruction in the startup path.
+    flutter_controller_ = std::make_unique<flutter::FlutterViewController>(
+        frame.right - frame.left, frame.bottom - frame.top, project_);
+    // Ensure that basic setup of the controller was successful.
+    if (!flutter_controller_->engine() || !flutter_controller_->view())
+    {
+        return false;
+    }
+    RegisterPlugins(flutter_controller_->engine());
 
-  flutter_controller_->engine()->SetNextFrameCallback([&]() {
-    this->Show();
-  });
+    // [1] domain前缀 + channel名称
+    flutter::MethodChannel<> testChannel(flutter_controller_->engine()->messenger(), "test", &flutter::StandardMethodCodec::GetInstance());
+    flutter::MethodChannel<> batteryChannel(flutter_controller_->engine()->messenger(), "samples.flutter.dev/battery", &flutter::StandardMethodCodec::GetInstance());
+    // [2] channel方法
+    testChannel.SetMethodCallHandler([](const flutter::MethodCall<>& call, std::unique_ptr<flutter::MethodResult<>> result)
+        {
+            if (call.method_name() == "getSystemClock")
+            {
+                //std::time_t t = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+                std::timespec ts;
+                std::timespec_get(&ts, TIME_UTC);
+                result->Success(std::vector<int64_t>{ts.tv_sec, ts.tv_nsec});
+            }
+            else
+            {
+                result->NotImplemented();
+            }
+        });
+    batteryChannel.SetMethodCallHandler([](const flutter::MethodCall<>& call, std::unique_ptr<flutter::MethodResult<>> result)
+        {
+            if (call.method_name() == "getBatteryLevel")
+            {
+                int battery_level = GetBatteryLevel();
+                if (battery_level != -1)
+                {
+                    result->Success(battery_level);
+                }
+                else
+                {
+                    result->Error("UNAVAILABLE", "Battery level not available.");
+                }
+            }
+            else
+            {
+                result->NotImplemented();
+            }
+        });
 
-  return true;
+    SetChildContent(flutter_controller_->view()->GetNativeWindow());
+
+    flutter_controller_->engine()->SetNextFrameCallback([&]() {
+        this->Show();
+        });
+
+    return true;
 }
 
 void FlutterWindow::OnDestroy() {
-  if (flutter_controller_) {
-    flutter_controller_ = nullptr;
-  }
+    if (flutter_controller_) {
+        flutter_controller_ = nullptr;
+    }
 
-  Win32Window::OnDestroy();
+    Win32Window::OnDestroy();
 }
 
 LRESULT
 FlutterWindow::MessageHandler(HWND hwnd, UINT const message,
-                              WPARAM const wparam,
-                              LPARAM const lparam) noexcept {
-  // Give Flutter, including plugins, an opportunity to handle window messages.
-  if (flutter_controller_) {
-    std::optional<LRESULT> result =
-        flutter_controller_->HandleTopLevelWindowProc(hwnd, message, wparam,
-                                                      lparam);
-    if (result) {
-      return *result;
+    WPARAM const wparam,
+    LPARAM const lparam) noexcept {
+    // Give Flutter, including plugins, an opportunity to handle window messages.
+    if (flutter_controller_) {
+        std::optional<LRESULT> result =
+            flutter_controller_->HandleTopLevelWindowProc(hwnd, message, wparam,
+                lparam);
+        if (result) {
+            return *result;
+        }
     }
-  }
 
-  switch (message) {
+    switch (message) {
     case WM_FONTCHANGE:
-      flutter_controller_->engine()->ReloadSystemFonts();
-      break;
-  }
+        flutter_controller_->engine()->ReloadSystemFonts();
+        break;
+    }
 
-  return Win32Window::MessageHandler(hwnd, message, wparam, lparam);
+    return Win32Window::MessageHandler(hwnd, message, wparam, lparam);
 }
